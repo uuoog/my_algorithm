41c40dfeffd96e980dcdbe355e7dafe8c4f0be48
이승욱 <131653525+uuoog@users.noreply.github.com>
2023-06-30 09:32:11 +0900

    Time: 39 ms (95.44%), Space: 16.4 MB (28.35%) - LeetHub

--- 1329-minimum-cost-to-move-chips-to-the-same-position/1329-minimum-cost-to-move-chips-to-the-same-position.py
+++ 1329-minimum-cost-to-move-chips-to-the-same-position/1329-minimum-cost-to-move-chips-to-the-same-position.py
@@ -1,6 +1,5 @@
 class Solution:
     def minCostToMoveChips(self, position: List[int]) -> int:
-        cost = 0
         even_cnt = []
         odd_cnt = []
         for i in range(len(position)):
@@ -8,10 +7,5 @@ class Solution:
                 even_cnt.append(position[i])
             if position[i] % 2 == 1:
                 odd_cnt.append(position[i])
-        print(even_cnt, odd_cnt)
         cost = min(len(even_cnt), len(odd_cnt))
         return cost
-
-
-
-           
