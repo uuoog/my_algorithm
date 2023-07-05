0aaca276f605f8a73f069d0b2ca3542e344e7701
이승욱 <131653525+uuoog@users.noreply.github.com>
2023-07-05 09:14:58 +0900

    Time: 104 ms (8.11%), Space: 16.5 MB (9.69%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.py
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.py
@@ -1,8 +1,6 @@
 class Solution:
     def maxScore(self, s: str) -> int:
         answer = []
-        l_score = 0
-        r_score = 0
         c = [c for c in s]
         for i in range(1, len(c)):
             c[:i].count("0")
