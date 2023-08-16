f4ce842f5b8e44ad2bdc06ee3ae5bbc1f1b50dfd
이승욱 <131653525+uuoog@users.noreply.github.com>
2023-08-16 09:11:26 +0900

    Time: 39 ms (83.88%), Space: 16.3 MB (25.73%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -4,6 +4,6 @@ class Solution:
         if n <= 0:
             return dp[n]
         
-        for i in range(n - 1):
-            dp.append(dp[i] + dp[i+1])
+        for i in range(2, n+1):
+            dp.append(dp[i - 1] + dp[i - 2])
         return dp[-1]
