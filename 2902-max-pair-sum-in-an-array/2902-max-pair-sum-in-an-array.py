0d6bb555995ad695ae3fa14e0fc3e47ba197a361
이승욱 <131653525+uuoog@users.noreply.github.com>
2023-08-17 09:41:23 +0900

    Time: 1560 ms (5.01%), Space: 16.3 MB (58.01%) - LeetHub

---
+++ 2902-max-pair-sum-in-an-array/2902-max-pair-sum-in-an-array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxSum(self, nums: List[int]) -> int:
+        ans = -1
+
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                num_a = nums[i]
+                num_b = nums[j]
+                num_a_arr = [int(c) for c in str(num_a)]
+                num_b_arr = [int(c) for c in str(num_b)]
+
+                if max(num_a_arr) == max(num_b_arr):
+                    ans = max(ans, int(''.join(map(str, num_a_arr))) + int(''.join(map(str, num_b_arr))))
+
+        return ans
