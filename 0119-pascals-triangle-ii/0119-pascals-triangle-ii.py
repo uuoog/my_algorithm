5b26cb28fa34e9979ac489b15bb94c3f7e9f1f1c
이승욱 <131653525+uuoog@users.noreply.github.com>
2023-08-21 09:36:02 +0900

    Time: 45 ms (46.97%), Space: 16.4 MB (34.48%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -1,8 +1,6 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
-        answer = []
         dp = [1]
-        answer.append(dp)
         for i in range(1, rowIndex+1):
             new_dp = []
             for j in range(i+1):
@@ -10,6 +8,5 @@ class Solution:
                     new_dp.append(1)
                 else:
                     new_dp.append(dp[j-1] + dp[j])
-            answer.append(new_dp)
             dp = new_dp
-        return answer[rowIndex]
+        return dp
